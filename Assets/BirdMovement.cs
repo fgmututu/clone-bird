07ab4bfade1b5d7ed57350ef9c6579e55820f1b6
fgmututu <mututufan@gmail.com>
2016-06-29 01:48:20 -0700

    clone bird v1.0

---
+++ Assets/BirdMovement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class BirdMovement : MonoBehaviour {
+
+//	Vector3 velocity = Vector3.zero;
+	public float flapSpeed    = 150f;
+	float forwardSpeed = 1f;
+
+	bool didFlap = false;
+	Rigidbody2D player;
+	Animator animator;
+
+	public bool dead = false;
+	float deathCooldown;
+	string sceneName;
+
+	public bool godMode = false;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody2D> ();
+		animator = transform.GetComponentInChildren<Animator> ();
+		sceneName = SceneManager.GetActiveScene ().name;
+	}
+
+	// Do Graphic & Input updates here
+	void Update() {
+		if (dead) {
+			deathCooldown -= Time.deltaTime;
+
+			if (deathCooldown <= 0) {
+				if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+					SceneManager.LoadScene (sceneName, LoadSceneMode.Single);
+				}
+			}
+		} else {
+			if (Input.GetKeyDown (KeyCode.Space) || Input.GetMouseButtonDown (0)) {
+				didFlap = true;
+			}
+		}
+	}
+	
+	// Do physics engine updates here
+	void FixedUpdate () {
+
+		if (dead)
+			return;
+		
+		player.AddForce ( Vector2.right * forwardSpeed );
+
+		if (didFlap) {
+			player.AddForce ( Vector2.up * flapSpeed );
+
+			animator.SetTrigger ("DoFlap");
+			didFlap = false;
+		}
+
+		if (player.velocity.y > 0) {
+			transform.rotation = Quaternion.Euler (0, 0, 0);
+		}
+		else {
+			float angle = Mathf.Lerp (0, -90, (-player.velocity.y / 3f));
+			transform.rotation = Quaternion.Euler (0, 0, angle);
+
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		if (godMode)
+			return;
+		
+		animator.SetTrigger ("Death");
+		dead = true;
+		deathCooldown = 0.5f;
+	}
+}
---
+++ Assets/Score.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	static int score = 0;
+	static int highScore = 0;
+
+	static Score instance;
+
+	GUIText text;
+
+	BirdMovement bird;
+
+	void Start(){
+		instance = this;
+
+		text = GetComponent<GUIText> ();
+
+		GameObject player_go = GameObject.FindGameObjectWithTag ("Player");
+		if (player_go == null) {
+			Debug.LogError ("Could not find an object with tag 'Player'.");
+		}
+		bird = player_go.GetComponent<BirdMovement> ();
+	
+		score = 0;
+
+		highScore = PlayerPrefs.GetInt ("highScore", 0);
+	}
+
+	static public void AddPoint() {
+		if (instance.bird.dead)
+			return;
+
+		score++;
+
+		if (score > highScore) {
+			highScore = score;
+		}
+	}
+
+	void OnDestroy() {
+		instance = null;
+		PlayerPrefs.SetInt ("highScore", highScore);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Score: " + score + "\n High Score: " + highScore;
+	}
+}
